d5aefe6259de028a5487c1b97941424bc377b863
sajal12092001 <sajalsharma.1209@gmail.com>
2022-06-30 14:44:56 +0530

    BCA College Android Project

---
+++ app/src/main/java/com/example/saviour/Main_Activity/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.saviour;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tablayout;
+    TabItem tabitem1,tabitem2,tabitem3;
+    ViewPager viewpager1;
+    PageAdapter pageadapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tablayout= findViewById(R.id.tablayout1);
+        tabitem1= findViewById(R.id.tabitem1);
+        tabitem2= findViewById(R.id.tabitem2);
+        tabitem3= findViewById(R.id.tabitem3);
+        viewpager1= findViewById(R.id.viewpager1);
+
+        pageadapter = new PageAdapter(getSupportFragmentManager(),tablayout.getTabCount());
+        viewpager1.setAdapter(pageadapter);
+
+        tablayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+             viewpager1.setCurrentItem(tab.getPosition());
+
+             if(tab.getPosition()==0||tab.getPosition()==1||tab.getPosition()==2)
+             {
+                 pageadapter.notifyDataSetChanged();
+             }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewpager1.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tablayout));
+    }
+}
---
+++ app/src/main/java/com/example/saviour/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.example.saviour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreen extends AppCompatActivity {
+    ImageView imageView;
+    TextView title,bottomtitle;
+    Animation top,middle,bottom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        imageView=findViewById(R.id.logo);
+        title=findViewById(R.id.title);
+        bottomtitle=findViewById(R.id.bottomtitle);
+
+        top= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.top);
+        middle= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.middle);
+        bottom= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bottom);
+
+        imageView.setAnimation(top);
+        title.setAnimation(middle);
+        bottomtitle.setAnimation(bottom);
+
+        new Handler().postDelayed(() -> {
+
+            startActivity(new Intent(SplashScreen.this,MainActivity.class));
+            finish();
+        },3000);
+    }
+}
