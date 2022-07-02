f08fbe6aca32c5db6f4214cd94369bd15b5c5eec
sajal <sajalsharma.1209@gmail.com>
2022-07-02 19:23:09 +0530

    todayupdate

---
+++ app/src/main/java/com/example/saviour/Conn.java
@@ -0,0 +1,42 @@
+package com.example.saviour;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Conn extends SQLiteOpenHelper {
+    private  static final String databasename="database.db";
+    public Conn(@Nullable Context context) {
+        super(context,databasename,null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE members (\n" +
+                "    id          INTEGER PRIMARY KEY ASC AUTOINCREMENT\n" +
+                "                        NOT NULL,\n" +
+                "    name      STRING  NOT NULL,\n" +
+                "    mobile        STRING  NOT NULL);");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public boolean insert_members(String name,String mobile)
+    {
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues c=new ContentValues();
+        c.put("name",name);
+        c.put("mobile",mobile);
+
+        long r=db.insert("members",null,c);
+
+        return r!=-1;
+    }
+}
--- app/src/main/java/com/example/saviour/Main_Activity/Register_Number/Add_Number.java
+++ app/src/main/java/com/example/saviour/Main_Activity/Register_Number/Add_Number.java
@@ -3,7 +3,12 @@ package com.example.saviour.Main_Activity.Register_Number;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.saviour.Conn;
 import com.example.saviour.R;
 
 import java.util.Objects;
@@ -12,9 +17,44 @@ public class Add_Number extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EditText name,mobile;
+        Button submit;
+        Conn conn=new Conn(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_number);
         setTitle("Add Members");
+        name=findViewById(R.id.name);
+        mobile=findViewById(R.id.mobile);
+        submit=findViewById(R.id.submit);
+
+        submit.setOnClickListener(view ->{
+
+            String mem_name=name.getText().toString().trim().toUpperCase();
+            String mem_mobile=mobile.getText().toString().trim();
+            if(mem_name.isEmpty() && mem_mobile.isEmpty())
+            {
+                name.setError("Field Required");
+                mobile.setError("Field Required");
+            }
+            else if(mem_name.isEmpty())
+            {
+                name.setError("Field Required");
+            }
+            else if(mem_mobile.isEmpty())
+            {
+                mobile.setError("Field Required");
+            }
+            else {
+
+                boolean check = conn.insert_members(mem_name, "+91-" + mem_mobile);
+                if (check) {
+                    Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show();
+                    name.setText("");
+                    mobile.setText("");
+                }
+            }
+
+        });
 
     }
 }
